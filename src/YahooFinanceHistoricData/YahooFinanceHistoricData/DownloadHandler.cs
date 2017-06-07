76546156ef2d2ebf834af6fec0e1b3f89913e6f5
RedSpiderMkV <redspidermkv@hotmail.com>
2017-06-07 22:32:36 +0100

    initial commit - work in progress, proof of concept

---
+++ src/YahooFinanceHistoricData/YahooFinanceHistoricData/DownloadHandler.cs
@@ -0,0 +1,51 @@
+﻿// Copyright © 2010-2017 The CefSharp Authors. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
+using System;
+using System.IO;
+
+namespace CefSharp.Example
+{
+    public class DownloadHandler : IDownloadHandler
+    {
+        public event EventHandler<DownloadItem> OnBeforeDownloadFired;
+
+        public event EventHandler<DownloadItem> OnDownloadUpdatedFired;
+
+        public DownloadHandler(string downloadDirectory, string fileName)
+        {
+            _fileName = fileName;
+            _directoryName = downloadDirectory;
+        }
+
+        public void OnBeforeDownload(IBrowser browser, DownloadItem downloadItem, IBeforeDownloadCallback callback)
+        {
+            var handler = OnBeforeDownloadFired;
+            if (handler != null)
+            {
+                handler(this, downloadItem);
+            }
+
+            if (!callback.IsDisposed)
+            {
+                using (callback)
+                {
+                    callback.Continue(Path.Combine(_directoryName, _fileName), false);
+                }
+            }
+        }
+
+        public void OnDownloadUpdated(IBrowser browser, DownloadItem downloadItem, IDownloadItemCallback callback)
+        {
+            if(downloadItem.IsComplete)
+            {
+                EventHandler<DownloadItem> handler = OnDownloadUpdatedFired;
+                handler?.Invoke(this, downloadItem);
+            }
+        }
+
+        private readonly string _directoryName;
+        private readonly string _fileName;
+    }
+}
---
+++ src/YahooFinanceHistoricData/YahooFinanceHistoricData/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+using YahooFinanceBrowserManipulator2;
+
+namespace YahooFinanceHistoricData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string symbol = args[0];
+            string name = args[1];
+
+            Console.WriteLine("Starting...");
+            
+            Application.Run(new BrowserManipulator(symbol, name));
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
